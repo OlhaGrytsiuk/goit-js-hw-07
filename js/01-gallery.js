cf2c34ec83fb301524bfff4ce2d9ac773b0a62ee
olhagrytsiuk <oljazozylja@gmail.com>
2023-04-03 15:46:10 +0300

    home work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryImages = document.querySelector('.gallery');
+
+function galleryMarkup (items) {
+    return items.map((item) => `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`).join("");
+}
+
+const addGalleryMarkup = galleryMarkup(galleryItems);
+galleryImages.innerHTML = addGalleryMarkup;
+
+galleryImages.addEventListener("click", onClick);
+
+function onClick(event) {
+    event.preventDefault();
+
+    if(event.target.nodeName !== "IMG"){
+        return;
+    }
+
+    const intanse = basicLightbox.create(`
+    <img src = "${event.target.dataset.source}" width = "800" height = "600">
+    `);
+    intanse.show();
+
+    galleryImages.addEventListener("keydown", (event) => {
+        if(event.code === "Escape"){
+            intanse.close();
+        }
+    })
+
+}
+
+
+
